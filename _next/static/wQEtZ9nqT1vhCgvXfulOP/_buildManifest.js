6b0324b9c6d99abf2ec610fc1d778c65ad104dd3
QuocNguyen612k <QuocNguyen612k@users.noreply.github.com>
2021-08-08 12:23:54 +0000

    Deploying to gh-pages from @ QuocNguyen612k/EMenu@867f85aed08fceb941ba019cb52cdce8c8bb1db6 🚀

--- _next/static/wQEtZ9nqT1vhCgvXfulOP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-9700cd18fc01feb4d9e2.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
