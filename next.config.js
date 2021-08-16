68e37c7366bc2d364ff8a374ec8ce781807c104c
Nguyen Quoc <quocnguyen612k@gmail.com>
2021-08-16 21:33:56 +0700

    Update next.config.js
    Enable trailing slash to fix 404 dynamic route when deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  exportTrailingSlash: true,
   images: {
     loader: "imgix",
     path: "",
