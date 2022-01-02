532a349bf1db4d8321a0d27b78f157fdc46393c6
Phan Minh Hoang <hoangpm22@gmail.com>
2022-01-02 18:35:17 +0700

    add AddCategory feature

--- components/AdminPage/MenuManagement/CategoryManagement/index.tsx
+++ components/AdminPage/MenuManagement/CategoryManagement/index.tsx
@@ -1,16 +1,8 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardHeader,
-  CardActions,
-  FormControl,
-  OutlinedInput,
-} from '@material-ui/core'
-import { useStyles } from '@/styles/modal'
+import { Card, CardHeader } from '@material-ui/core'
 import { Category } from '@/models/place'
 import DeleteCategory from './DeleteCategory'
 import UpdateCategory from './UpdateCategory'
+import AddCategory from './AddCategory'
 
 interface Props {
   categories: Category
@@ -18,29 +10,11 @@ interface Props {
 }
 
 const CategoryManagement = ({ categories, placeID }: Props) => {
-  const classes = useStyles()
-
   return (
     <>
       <Card variant="outlined" style={{ margin: '1% 1% 1% 0' }}>
         <CardHeader title="Thêm loại" />
-        <form>
-          <Box display="flex" style={{ margin: '0 0 1% 1%' }}>
-            <FormControl margin="dense">
-              <OutlinedInput required name="newCategory" />
-            </FormControl>
-            <CardActions>
-              <Button
-                size="large"
-                type="submit"
-                className={classes.buttonLineGradient}
-                variant="contained"
-              >
-                Xác nhận
-              </Button>
-            </CardActions>
-          </Box>
-        </form>
+        <AddCategory categories={categories} placeID={placeID} />
       </Card>
       <Card variant="outlined" style={{ margin: '1% 1% 1% 0' }}>
         <CardHeader title="Sửa loại" />
