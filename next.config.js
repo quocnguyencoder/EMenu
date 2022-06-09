e7d3d9db5d69969ef3eb98bc48d354445ff511e5
quocnguyencoder <quocnguyen612k@gmail.com>
2022-06-09 19:08:26 +0700

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     loader: 'imgix',
     path: '',
   },
-}
+});
