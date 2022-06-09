5a1d67f47a098342c17294f1e2366cd97f7d4ca4
quocnguyencoder <quocnguyen612k@gmail.com>
2022-06-09 19:59:45 +0700

    update link to manifest.json

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
-const withPWA = require("next-pwa");
+const withPWA = require('next-pwa')
 
 module.exports = withPWA({
   pwa: {
-    dest: "public",
+    dest: 'public',
     register: true,
     skipWaiting: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  
   images: {
     loader: 'imgix',
     path: '',
   },
-});
+})
